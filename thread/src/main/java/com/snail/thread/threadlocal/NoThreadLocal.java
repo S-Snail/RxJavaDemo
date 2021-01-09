523286bca99d3bee6c8df62d7a719e3935d94556
S-Snail <1114979847@qq.com>
2021-01-09 17:18:24 +0800

    test no use threadlocal

---
+++ thread/src/main/java/com/snail/thread/threadlocal/NoThreadLocal.java
@@ -0,0 +1,35 @@
+package com.snail.thread.threadlocal;
+
+public class NoThreadLocal {
+    static Integer count = new Integer(1);
+    private void runThreeThread(){
+        Thread[] threads = new Thread[3];
+        for (int i = 0; i < 3; i++) {
+            threads[i] = new Thread(new TestTask(i));
+        }
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+    }
+
+    public static class TestTask implements Runnable{
+
+        private int id;
+
+        public TestTask(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " : start");
+            count = count + id;
+            System.out.println(Thread.currentThread().getName() + " count = " + count);
+        }
+    }
+
+    public static void main(String[] args) {
+        NoThreadLocal noThreadLocal = new NoThreadLocal();
+        noThreadLocal.runThreeThread();
+    }
+}
